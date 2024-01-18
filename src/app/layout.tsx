f040e41fec11c80c527a904822c5b6572ddde70d
masha-girya <teenderrr@gmail.com>
2024-01-18 14:00:08 +0200

    seo test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,25 @@ import "./globals.scss";
 import type { Metadata } from "next";
 import { Cormorant } from "next/font/google";
 import { Header, Footer, Container } from "@/components";
+import Favicon from '/public/favicon.ico';
 
 const inter = Cormorant({ subsets: ["cyrillic", "latin"] });
 
 export const metadata: Metadata = {
-  title: "Thats What She Baked",
-  description: "Цей кекс точно буде смачним та красивим!",
+  title: "Thats What She Baked | Ось, що вона спекла",
+  description: "Детальні рецепти, щоб надихнутись своїм творінням!",
+  icons: [{ rel: 'icon', url: Favicon.src }],
+  keywords: ["рецепти", "рецепт", "рецепт чізкейку", "чізкейк рецепт", "випічка рецепти", "кекс рецепти" ],
+  twitter: {
+    description: "Детальні рецепти, щоб надихнутись своїм творінням!",
+    title: "Thats What She Baked | Ось, що вона спекла",
+    images: "https://res.cloudinary.com/dqkoxlze0/image/upload/v1690967129/bakery/cheesecake%20Friends/photo_2023-07-01_20-33-56_t1oko7.jpg",
+  },
+  openGraph: {
+    description: "Детальні рецепти, щоб надихнутись своїм творінням!",
+    title: "Thats What She Baked | Ось, що вона спекла",
+    images: "https://res.cloudinary.com/dqkoxlze0/image/upload/v1690967129/bakery/cheesecake%20Friends/photo_2023-07-01_20-33-56_t1oko7.jpg",
+  }
 };
 
 export default function RootLayout({
@@ -16,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ua">
+    <html lang="uk">
       <body>
         <Header />
           <Container>
